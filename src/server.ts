f2e4e7e159dcf483ff2f8af662d1b6520c7c55bc
dracula <adamportifolio@gmail.com>
2022-05-13 21:07:36 -0300

    Criando Middleware de errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 
@@ -7,10 +8,18 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.get("/", (req, res)=>{
-    return res.json({
-        message: "hello World Isadora"
-    });
-});
+app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
+    if(err instanceof Error){
+        return response.status(400).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error."
+    })
+
+})
 
 app.listen(3333, () => console.log("Server is running"))
